7ffb3f3491c8b7e369fe5b047ca32f83ad8b4246
r00t-O-c0ll1n5 <saegecollins@gmail.com>
2024-06-03 16:27:17 +0300

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["Speed", "Flexibility", "Reusbility", "Performance", "Rich"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
       {" "}
-      <ListGroup items={items} heading="Top Perks" />
+      <ListGroup
+        items={items}
+        heading="Top Perks"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
