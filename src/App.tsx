a0cdbefb5ad9fac6e47d677a3293a532299812e8
r00t-O-c0ll1n5 <saegecollins@gmail.com>
2024-06-03 18:03:42 +0300

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import Uses from "./components/Uses";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
-      <Uses>
-        Top 5 <span> Real World Applications </span>{" "}
-      </Uses>
+      {alertVisible && (
+        <Alert
+          onClose={() => setAlertVisible(false)}
+          children="You have Clicked Me, Now Give me a Snack!"
+        ></Alert>
+      )}
+
+      <Button color="success" onClick={() => setAlertVisible(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
